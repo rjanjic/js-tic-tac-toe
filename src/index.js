0fd1863fde98ed6db2c86d923f9943bcce40ffde
Radovan Janjic <radovan.janjic@trivago.com>
2017-09-11 21:35:02 +0200

    Added AI player.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,123 @@
 import './index.scss';
 
-const xWinsEl = document.getElementById('js-x-wins');
-const oWinsEl = document.getElementById('js-o-wins');
+const humanWinsEl = document.getElementById('js-human-wins');
+const aiWinsEl = document.getElementById('js-ai-wins');
 
 let state;
-let turn;
-let map;
+let map = [null, null, null, null, null, null, null, null, null];
 let win;
 const games = [];
 
-const isGameFinished = () => {
-    // rows
+const getAvailableFields = board => {
+    const indexes = [];
+    for (let i = 0; i < 9; i++) {
+        if (board[i] === null) {
+            indexes.push(i);
+        }
+    }
+    return indexes;
+};
+
+let HUMAN = 'X';
+let AI = 'O';
+
+function winning(board, player) {
     for (let i = 0; i <= 6; i = i + 3) {
-        if (map[i] !== null && map[i] === map[i + 1] && map[i] === map[i + 2]) {
-            state = map[i];
+        if (player === board[i] && player === board[i + 1] && player === board[i + 2]) {
+            return true;
+        }
+    }
+    for (let i = 0; i <= 2 ; i++) {
+        if (player === board[i] && player === board[i + 3] && player === board[i + 6]) {
+            return true;
+        }
+    }
+    for (let i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+        if (player === board[i] && player === board[i + j] && player === board[i + 2 * j]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function miniMax(map, player) {
+    const board = map.slice();
+    const available = getAvailableFields(board);
+
+    if (winning(board, HUMAN)) {
+        return {
+            score: -10
+        };
+    } else if (winning(board, AI)) {
+        return {
+            score: 10
+        };
+    } else if (available.length === 0) {
+        return {
+            score: 0
+        };
+    }
+
+    const moves = [];
+    for (let i = 0; i < available.length; i++) {
+        const move = {};
+        move.index = available[i];
+        board[available[i]] = player;
+
+        if (player === AI) {
+            move.score = miniMax(board, HUMAN).score;
+        } else {
+            move.score = miniMax(board, AI).score;
+        }
+        board[available[i]] = null;
+        moves.push(move);
+    }
+
+    let bestMove;
+    if (player === AI) {
+        let bestScore = -10000;
+        for (let i = 0, l = moves.length; i < l; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        let bestScore = 10000;
+        for (let i = 0, l = moves.length; i < l; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
+}
+
+const isGameFinished = board => {
+    for (let i = 0; i <= 6; i = i + 3) {
+        if (board[i] !== null && board[i] === board[i + 1] && board[i] === board[i + 2]) {
+            state = board[i];
             win.push(i, i + 2);
             return true;
         }
     }
-
-    // columns
     for (let i = 0; i <= 2 ; i++) {
-        if (map[i] !== null && map[i] === map[i + 3] && map[i] === map[i + 6]) {
-            state = map[i];
+        if (board[i] !== null && board[i] === board[i + 3] && board[i] === board[i + 6]) {
+            state = board[i];
             win.push(i, i + 6);
             return true;
         }
     }
-
-    // diagonals
     for (let i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
-        if (map[i] !== null && map[i] === map[i + j] && map[i] === map[i + 2 * j]) {
-            state = map[i];
+        if (board[i] !== null && board[i] === board[i + j] && board[i] === board[i + 2 * j]) {
+            state = board[i];
             win.push(i, i + 2 * j);
             return true;
         }
     }
-
     for (let i = 0; i < 9 ; i++) {
-        if (map[i] === null) {
+        if (board[i] === null) {
             return false;
         }
     }
@@ -47,23 +126,38 @@ const isGameFinished = () => {
     return true;
 };
 
+const makeAImove = containerEl => {
+    const AImove = miniMax(map, AI);
+    map[AImove.index] = AI;
+    if (AImove.index !== undefined) {
+        containerEl.children[AImove.index].classList.add(`field--${AI}`);
+    }
+};
+
 const handleClick = (containerEl, el, index) => () => {
     if (map[index] !== null) return;
-    el.classList.add(`field--${turn}`);
-    map[index] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
-    if (isGameFinished()) {
+    el.classList.add(`field--${HUMAN}`);
+    map[index] = HUMAN;
+
+    makeAImove(containerEl);
+
+    if (isGameFinished(map)) {
+        containerEl.classList.add('container--finished');
         const winEl = document.createElement('span');
-        if (state === 'X') {
-            xWinsEl.appendChild(winEl);
+        if (state === HUMAN) {
+            humanWinsEl.appendChild(winEl);
         }
-        if (state === 'O') {
-            oWinsEl.appendChild(winEl);
+
+        if (state === AI) {
+            aiWinsEl.appendChild(winEl);
         }
 
         if (state === 'X' || state === 'O') {
             containerEl.classList.add('container--win', `container--${win[0]}-${win[1]}`);
         }
+
+        HUMAN = HUMAN === 'X' ? 'O' : 'X';
+        AI = AI === 'X' ? 'O' : 'X';
         startGame();
     }
 };
@@ -83,7 +177,6 @@ const getRandomPosition = (rows, cols) => {
 const startGame = () => {
     win = [];
     state = 'ongoing';
-    turn = 'X';
     map = [null, null, null, null, null, null, null, null, null];
 
     const containerEl = document.createElement('div');
@@ -96,9 +189,9 @@ const startGame = () => {
         containerEl.appendChild(fieldEl);
     }
 
-
     const cols = ~~(window.innerHeight / 150);
     const rows = ~~(window.innerWidth / 150);
+    if (games.length === cols * rows - 1) return;
     const position = getRandomPosition(rows, cols);
 
     containerEl.style.left = `${position.y * 150}px`;
@@ -106,6 +199,10 @@ const startGame = () => {
     containerEl.style.transform = `rotate(${Math.random() >= 0.5 ? '-' : ''}${Math.floor(Math.random() * 30)}deg)`;
 
     document.body.appendChild(containerEl);
+
+    if (AI === 'X') {
+        makeAImove(containerEl);
+    }
 };
 
 startGame();
